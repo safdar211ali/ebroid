5d26e3933cc6c7ddfa93afc99b08f8ba3b1ee9dc
ali <safdar211@hotmail.com>
2015-10-16 08:15:39 +0500

    second commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  before_action :set_store, only: [:show]
   def index
     @store = Design.all
+  end
+
+  def show
+
+  end
+  private
+  def set_store
+    @store = Design.find(params[:id])
   end
 end
